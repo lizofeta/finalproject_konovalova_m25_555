7537069af7ed2185dcd3293f9b2cd5ce1e0f8fdf
Elizaveta Konovalova <Eliza050203@yandex.ru>
2025-11-15 13:44:08 +0000

    added new constants: file paths
    in module constants.py added three new constants:
    - filepath to users.json
    - filepath to portfolios.json
    - filepath to rates.json

--- valutatrade_hub/core/constants.py
+++ valutatrade_hub/core/constants.py
@@ -1,2 +1,6 @@
 BASE_URL_EXCHANGE_RATE = 'https://v6.exchangerate-api.com/v6/'
 API_KEY = '5846b5fe25cd7b2e13d6ae0b'
+
+USER_DATA_PATH = 'data/users.json'
+PORTFOLIOS_DATA_PATH = 'data/portfolios.json'
+RATES_DATA_PATH = 'data/rates.json'
